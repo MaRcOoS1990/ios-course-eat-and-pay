7030acf80551c36d74ef14e6d40d859617d6ce1e
Aleksey Chalov <chalov.aleksey5@rwb.ru>
2026-07-03 17:14:35 +0300

    Initial Commit

---
+++ EatAndPay/EatAndPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  EatAndPayApp.swift
+//  EatAndPay
+//
+//  Created by Чалов Алексей Вячеславович on 03.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct EatAndPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EatAndPayTests/EatAndPayTests.swift
@@ -0,0 +1,17 @@
+//
+//  EatAndPayTests.swift
+//  EatAndPayTests
+//
+//  Created by Чалов Алексей Вячеславович on 03.07.2026.
+//
+
+import Testing
+@testable import EatAndPay
+
+struct EatAndPayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
